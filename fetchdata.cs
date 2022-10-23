35beba284fa15933f0838928cf643696db4cf2f1
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-23 14:44:07 +0530

    checkfetch

---
+++ fetchdata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InventoryManagement213
+{
+    internal class fetchdata
+    {
+        public  inventory Read(string path )
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(path))
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<inventory>(json);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
