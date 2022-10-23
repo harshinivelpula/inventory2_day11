147d1517d0fb950b1cfd1725b9c30ef7689cdb84
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-23 14:43:21 +0530

    checkinventory

---
+++ inventory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement213
+{
+    internal class inventory
+    {
+        public List<TypesofRice> typesofrice;
+        public List<TypesofPulses> typesofpulses;
+        public List<TypesofWheat> typesofwheat;
+    }
+        public class TypesofRice
+        {
+            public string name;
+            public int weight;
+            public int price;
+        }
+    public class TypesofPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesofWheat
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+}
